e75ea9e59617bd14a31998564d64b6397c75cbc7
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-04-10 14:26:53 +0900

    A型事業所ページ

--- page-Office_A.php
+++ page-Office_A.php
@@ -10,12 +10,12 @@ Template Name: labwelA型事業所ページ
 
 <div class="container">
     <div class="row">
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 py-5">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt80 pb-5">
             <div class="text-center">
                 <img src="<?php echo esc_url( get_theme_file_uri('images/office_a-topimg.png') ); ?>" width="70%">
             </div>
-            <div class="py-5">
-                <p class="text-center font-weight-bolder font25">バリエーション豊富なお仕事内容だから利用者のやりがいに繋がるお仕事が見つかります。<br>やりがいのあるお仕事でとことん得意分野を伸ばしていただきます。</p>
+            <div class="pt-5">
+                <p class="text-center font-weight-bolder font25 a-toptext">バリエーション豊富なお仕事内容だから利用者のやりがいに繋がるお仕事が見つかります。<br>やりがいのあるお仕事でとことん得意分野を伸ばしていただきます。</p>
             </div>
         </div>
     </div>
@@ -25,8 +25,55 @@ Template Name: labwelA型事業所ページ
     <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-A-img1">
             <div class="a-box1">
-                <p class="text-center text-white font-weight-bolder font50">戸別収集</p>
+                <p class="text-center text-white font-weight-bolder a-box-title">戸別収集</p>
+                <p class="text-center text-white a-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
             </div>
+            <div class="a-box-img">
+                <img src="<?php echo esc_url( get_theme_file_uri('images/office_a-qr.png') ); ?>">
+            </div>
+            <a href="#" class="a-link"></a>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-A-img2">
+            <div class="a-box1">
+                <p class="text-center text-white font-weight-bolder a-box-title">海岸清掃</p>
+                <p class="text-center text-white a-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+            </div>
+            <a href="#" class="a-link"></a>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-A-img3">
+            <div class="a-box1">
+                <p class="text-center text-white font-weight-bolder a-box-title">高齢者生活支援</p>
+                <p class="text-center text-white a-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+            </div>
+            <a href="#" class="a-link"></a>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-A-img4">
+            <div class="a-box1">
+                <p class="text-center text-white font-weight-bolder a-box-title">洗濯・クリーニング</p>
+                <p class="text-center text-white a-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+            </div>
+            <a href="#" class="a-link"></a>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-A-img5">
+            <div class="a-box1">
+                <p class="text-center text-white font-weight-bolder a-box-title">寺院清掃</p>
+                <p class="text-center text-white a-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+            </div>
+            <a href="#" class="a-link"></a>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-A-img6">
+            <div class="a-box1">
+                <p class="text-center text-white font-weight-bolder a-box-title">お弁当の調理・販売</p>
+                <p class="text-center text-white a-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+            </div>
+            <a href="#" class="a-link"></a>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-A-img7">
+            <div class="a-box1">
+                <p class="text-center text-white font-weight-bolder a-box-title">ケータリング</p>
+                <p class="text-center text-white a-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+            </div>
+            <a href="#" class="a-link"></a>
         </div>
     </div>
 </div>
---
+++ page-Office_B.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Template Name: labwelB型事業所ページ
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt80 pb-5">
+            <div class="text-center">
+                <img src="<?php echo esc_url( get_theme_file_uri('images/office_b-topimg.png') ); ?>" width="70%">
+            </div>
+            <div class="pt-5">
+                <p class="text-center font-weight-bolder font25 b-toptext">毎日、みんなとお仕事をするのが楽しい！！<br>そんな働きやすい環境と人間関係づくりにこだわっています。<br>安心して働ける自分の居場所が見つかります。</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
