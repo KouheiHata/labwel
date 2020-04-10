6189078d1a091da766e24bf89e6b442bb289795d
yamaguchi371 <45989778+yamaguchi371@users.noreply.github.com>
2020-04-10 09:38:16 +0900

    セクション３作成
    セクション３部分を作成

--- front-page.php
+++ front-page.php
@@ -2,72 +2,77 @@
 
 <div class="wrapper">
     <main id="main">
-            
-                <section id="section01" class="section01" style="">
-                    <div class="bg">
-                        <div></div>
-                    </div>
-                    <div class="l-text01">
-                        <h1>
-                            <img src="<?php echo esc_url( get_theme_file_uri('images/l-text1.png') ); ?>" alt="Your specialty leads to happiness 得意を伸ばそう">
-                            <div></div>
-                        </h1>
-                        <div class="h1cover"></div>
-                    </div>
-                    <div class="carousel1">
-                        <div class="i-wrapper">
-                            <ul>
-                                <li><img src="<?php echo esc_url( get_theme_file_uri('images/top_image1.jpg') ); ?>" alt=""></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="carouselborder">
-                        <img src="<?php echo esc_url( get_theme_file_uri('images/border1_04.jpg') ); ?>" alt="">
-                    </div>
-                </section>
-            
-                <section id="section02" class="section02">
-                    <div class="bg">
-                        <div style="height:100%;"></div>
-                    </div>
-                    <div class="carousel2">
-                        <div class="i-wrapper">
-                            <ul>
-                                <li>
-                                    <img src="<?php echo esc_url( get_theme_file_uri('images/top_image02_01.jpg') ); ?>" alt="">
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="carouselborder">
-                            <div></div>
-                        </div>
-                    </div>
-                    <div class="carousel3">
-                        <div class="i-wrapper">
-                            <ul>
-                                <li>
-                                    <img src="<?php echo esc_url( get_theme_file_uri('images/top_image03.jpg') ); ?>" alt="">
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="carouselborder">
-                            <div></div>
-                        </div>
-                    </div>
-                    <div class="content">
-                        <p class="content-title">CONCEPT</p>
-                        <P class="content-text">株式会社LABwelは、お仕事を通して自分の存在価値・やる気新たな能力の発見など、利用者さんの成長に繋がる自立支援をサポートする会社です。</P>
-                    </div>
-                </section>
-            
-                <section id="section03"></section>
-            
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <section id="section04"></section>
+
+        <section id="section01" class="section01" style="">
+            <div class="bg">
+                <div></div>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <section id="section05"></section>
+            <div class="l-text01">
+                <h1>
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/l-text1.png') ); ?>" alt="Your specialty leads to happiness 得意を伸ばそう">
+                    <div></div>
+                </h1>
+                <div class="h1cover"></div>
             </div>
+            <div class="carousel1">
+                <div class="i-wrapper">
+                    <ul>
+                        <li><img src="<?php echo esc_url( get_theme_file_uri('images/top_image1.jpg') ); ?>" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="carouselborder">
+                <img src="<?php echo esc_url( get_theme_file_uri('images/border1_04.jpg') ); ?>" alt="">
+            </div>
+        </section>
+
+        <section id="section02" class="section02">
+            <div class="bg">
+                <div style="height:100%;"></div>
+            </div>
+            <div class="carousel2">
+                <div class="i-wrapper">
+                    <ul>
+                        <li>
+                            <img src="<?php echo esc_url( get_theme_file_uri('images/top_image02_01.jpg') ); ?>" alt="">
+                        </li>
+                    </ul>
+                </div>
+                <div class="carouselborder">
+                    <div></div>
+                </div>
+            </div>
+            <div class="carousel3">
+                <div class="i-wrapper">
+                    <ul>
+                        <li>
+                            <img src="<?php echo esc_url( get_theme_file_uri('images/top_image03.jpg') ); ?>" alt="">
+                        </li>
+                    </ul>
+                </div>
+                <div class="carouselborder">
+                    <div></div>
+                </div>
+            </div>
+            <div class="content">
+                <p class="content-title">CONCEPT</p>
+                <p class="content-text mt60">株式会社LABwelは、
+                    お仕事を通して自分の存在価値・やる気新たな能力の発見など、利用者さんの成長に繋がる自立支援をサポートする会社です。</p>
+                <a href="#" class="button">MORE</a>
+
+            </div>
+        </section>
+
+        <section id="section03">
+            <div class="movie">
+                <img src="<?php echo esc_url( get_theme_file_uri('images/movie_03.jpg') ); ?>" alt="">
+            </div>
+        </section>
+
+            <section id="section04"></section>
+
+            <section id="section05"></section>
+
 
     </main>
 </div>
