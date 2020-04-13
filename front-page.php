608e29b04c660c8f88886d91369958e0866d61f7
hirotaka <h_yamaguchi@standtogether-fukushi.com>
2020-04-13 17:56:32 +0900

    フロントページ4/13

--- front-page.php
+++ front-page.php
@@ -69,73 +69,78 @@
             </div>
         </section>
 
-            <section id="section04">
-                <div class="content">
-                    <p class="content-title">就労継続支援Ａ型</p>
-                    <h2>
-                        <img src="<?php echo esc_url( get_theme_file_uri('images/ipppo.png') ); ?>" alt="">
-                        <img src="<?php echo esc_url( get_theme_file_uri('images/+ipppo.png') ); ?>" alt="">
-                    </h2>
-                    <p class="content-text">バリエーション豊富なお仕事内容だから自分のやりがいにつながる得意なお仕事がきっと見つかります。</p>
-                    <a href="#" class="button">MORE</a>
-                    <div class="bg"></div>
-                    <ul>
-                        <li><img src="<?php echo esc_url( get_theme_file_uri('images/a_staff_img.jpg') ); ?>" alt=""></li>
-                        <li><img src="<?php echo esc_url( get_theme_file_uri('images/a_staff_img2.jpg') ); ?>" alt=""></li>
-                        <li><img src="<?php echo esc_url( get_theme_file_uri('images/a_staff_img3.jpg') ); ?>" alt=""></li>
-                    </ul>
-                </div>
-            </section>
-
-            <section id="section05">
+        <section id="section04">
+            <div class="bg"></div>
+            <div class="content">
+                <p class="content-title">就労継続支援Ａ型</p>
                 <h2>
-                    <img src="<?php echo esc_url( get_theme_file_uri('images/b_staff_img.jpg') ); ?>" alt="">
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/ipppo_06.png') ); ?>" alt="">
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/+ipppo_08.png') ); ?>" alt="">
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/a-copy_03.png') ); ?>" alt="">
                 </h2>
-                <div class="content">
-                    <p class="content-title">就労継続支援Ｂ型</p>
-                    <h3>
-                        <img src="<?php echo esc_url( get_theme_file_uri('images/+ipppo.png') ); ?>" alt="">
-                    </h3>
-                    <p class="content-text">みんなと仕事をするのが楽しくなる。そんな働きやすい環境と楽しい人間関係づくりにこだわりました。</p>
-                    <a href="#" class="button">MORE</a>
-                </div>
-            </section>
-            
-            <section id="section06">
-                <h3>利用者さんの日々の活動はこちら</h3>
-                <h4>【LABwelが“NHK”出演決定！？】</h4>date
-                <p>敦賀の就労支援継続A型B型事業所LABwelです。本日は日中の気温も２０度近く上がり気持ちのいい一日でしたね。さて表題の件ですがLABwelがNHKに出演が決定しました。（６月放送予定）LABwelは津敦賀の海岸清掃を敦賀市さんのサポートのもと１年を通して実施しております</p>
+                <p class="content-text">バリエーション豊富なお仕事内容だから
+                    自分のやりがいにつながる得意なお仕事が
+                    きっと見つかります。</p>
                 <a href="#" class="button">MORE</a>
-                
-                
-            </section>
-            
-            <section id="top-end">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 col-sm-4 colxs-4">
-                            <img src="<?php echo esc_url( get_theme_file_uri('images/labwel-logo.png') ); ?>" alt="">
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-4 colxs-4">
-                            
-                            <h3>
-                                <img src="<?php echo esc_url( get_theme_file_uri('images/ipppo_06.png') ); ?>" alt="ipppo!">
-                            </h3>
-                            <p>〒914-0045 敦賀市古田刈66-803-2</p>
-                            <p>TEL:0770-47-6526&nbsp;FAX:0770-47-6527</p>
-                            <p>TEL:0770-47-5024（おそうじ部）</p>
-                            
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-4 colxs-4">
-                            <h3>
-                                <img src="<?php echo esc_url( get_theme_file_uri('images/+ipppo_08.png') ); ?>" alt="+ipppo!">
-                            </h3>
-                            <p>〒914-0121&nbsp;福井県敦賀市野神2-34-2</p>
-                            <p>TEL:0770-47-6979&nbsp;FAX:0770-47-6984</p>
-                        </div>
+            </div>
+            <ul>
+                <li><img src="<?php echo esc_url( get_theme_file_uri('images/a_staff_img.jpg') ); ?>" alt=""></li>
+                <li><img src="<?php echo esc_url( get_theme_file_uri('images/a_staff_img2.jpg') ); ?>" alt=""></li>
+                <li><img src="<?php echo esc_url( get_theme_file_uri('images/a_staff_img3.jpg') ); ?>" alt=""></li>
+            </ul>
+        </section>
+
+        <section id="section05">
+            <div class="itemimage">
+                <img src="<?php echo esc_url( get_theme_file_uri('images/b_staff_img.jpg') ); ?>" alt="">
+            </div>
+           
+            <div class="content">
+                <p class="content-title">就労継続支援Ｂ型</p>
+                <h3>
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/+ipppo_08.png') ); ?>" alt="">
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/b-copy_07.png') ); ?>" alt="">
+                </h3>
+                <p class="content-text">みんなと仕事をするのが楽しくなる。そんな働きやすい環境と楽しい人間関係づくりにこだわりました。</p>
+                <a href="#" class="button">MORE</a>
+            </div>
+        </section>
+
+        <section id="section06">
+            <h3>利用者さんの日々の活動はこちら</h3>
+            <h4>【LABwelが“NHK”出演決定！？】</h4>date
+            <p>敦賀の就労支援継続A型B型事業所LABwelです。本日は日中の気温も２０度近く上がり気持ちのいい一日でしたね。さて表題の件ですがLABwelがNHKに出演が決定しました。（６月放送予定）LABwelは津敦賀の海岸清掃を敦賀市さんのサポートのもと１年を通して実施しております</p>
+            <a href="#" class="button">MORE</a>
+
+
+        </section>
+
+        <section id="top-end">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-4 colxs-4">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/labwel-logo.png') ); ?>" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 colxs-4">
+
+                        <h3>
+                            <img src="<?php echo esc_url( get_theme_file_uri('images/ipppo_06.png') ); ?>" alt="ipppo!">
+                        </h3>
+                        <p>〒914-0045 敦賀市古田刈66-803-2</p>
+                        <p>TEL:0770-47-6526&nbsp;FAX:0770-47-6527</p>
+                        <p>TEL:0770-47-5024（おそうじ部）</p>
+
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 colxs-4">
+                        <h3>
+                            <img src="<?php echo esc_url( get_theme_file_uri('images/+ipppo_08.png') ); ?>" alt="+ipppo!">
+                        </h3>
+                        <p>〒914-0121&nbsp;福井県敦賀市野神2-34-2</p>
+                        <p>TEL:0770-47-6979&nbsp;FAX:0770-47-6984</p>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
 
 
     </main>
