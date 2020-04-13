f486e32ffaf54ff506b01d48dc37ba3cef60e64b
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-04-13 16:01:33 +0900

    フッター

--- footer.php
+++ footer.php
@@ -1,5 +1,41 @@
 <footer id="footer">
-
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <div>
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/labwel-logo.png') ); ?>">
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <div>
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/ipppo_06.png') ); ?>" width="86px">
+                </div>
+                <div class="pt-2">
+                    <p class="font-weight-bolder">〒914-0045&nbsp;&nbsp;敦賀市古田刈66-803-2</p>
+                    <p class="font-weight-bolder">TEL:0770-47-6526&nbsp;&nbsp;FAX:0770-47-6527</p>
+                    <p class="font-weight-bolder">TEL:0770-47-5024（おそうじ部）</p>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <div>
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/+ipppo_08.png') ); ?>" width="86px">
+                </div>
+                <div class="pt-2">
+                    <p class="font-weight-bolder">〒914-0121&nbsp;&nbsp;福井県敦賀市野神2-34-2</p>
+                    <p class="font-weight-bolder">TEL:0770-47-6979&nbsp;&nbsp;FAX:0770-47-6984</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid py-3 bg-footer">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="text-center">
+                    <p class="d-inline-block text-left text-white">©︎&nbsp;&nbsp;2020,&nbsp;&nbsp;LABwel&nbsp;&nbsp;INC.,<br>ALL&nbsp;&nbsp;TRADEMARKS&nbsp;&nbsp;REGISTERED.&nbsp;&nbsp;ALL&nbsp;&nbsp;RIGHTS&nbsp;&nbsp;RESERVED</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer><!-- footer end -->
 
 <!--- bootstrap4 javascript --->
