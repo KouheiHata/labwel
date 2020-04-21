62d52dfc2331772311bbc9e1d63f809fa2f3e057
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-04-21 16:00:47 +0900

    B型レスポンシブ調整

--- page-Office_B.php
+++ page-Office_B.php
@@ -10,37 +10,37 @@ Template Name: labwelB型事業所ページ
 
 <div class="container">
     <div class="row">
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt80 pb-5">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt80 pb50 b-topbox">
             <div class="text-center">
                 <img src="<?php echo esc_url( get_theme_file_uri('images/office_b-topimg.png') ); ?>" width="70%">
             </div>
             <div class="pt-5">
-                <p class="text-center font-weight-bolder font25 b-toptext">毎日、みんなとお仕事をするのが楽しい！！<br>そんな働きやすい環境と人間関係づくりにこだわっています。<br>安心して働ける自分の居場所が見つかります。</p>
+                <p class="text-center font-weight-bolder font25 b-toptext">毎日、みんなとお仕事をするのが楽しい！！<br>そんな働きやすい環境と人間関係づくりにこだ<br class="d-none d-md-block d-lg-none">わっています。<br>安心して働ける自分の居場所が見つかります。</p>
             </div>
         </div>
     </div>
 </div>
 
-<div class="container-fluid">
+<div class="container-fluid b-wrapper">
     <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-B-img1">
             <div class="b-box">
-                <p class="text-center text-white font-weight-bolder b-box-title">結び昆布</p>
-                <p class="text-center text-white b-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+                <p class="text-white font-weight-bolder b-box-title">結び昆布</p>
+                <p class="text-white b-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br class="d-none d-lg-block">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br class="d-none d-lg-block">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
             </div>
             <a href="#" class="b-link"></a>
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-B-img2">
             <div class="b-box">
-                <p class="text-center text-white font-weight-bolder b-box-title">寺院清掃</p>
-                <p class="text-center text-white b-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+                <p class="text-white font-weight-bolder b-box-title">寺院清掃</p>
+                <p class="text-white b-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br class="d-none d-lg-block">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br class="d-none d-lg-block">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
             </div>
             <a href="#" class="b-link"></a>
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-B-img3">
             <div class="b-box">
-                <p class="text-center text-white font-weight-bolder b-box-title">施設清掃</p>
-                <p class="text-center text-white b-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br>テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
+                <p class="text-white font-weight-bolder b-box-title">施設清掃</p>
+                <p class="text-white b-box-text">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br class="d-none d-lg-block">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト<br class="d-none d-lg-block">テキストテキストテキストテキストテキストテキストテキストテキストテキストキスト</p>
             </div>
             <a href="#" class="b-link"></a>
         </div>
